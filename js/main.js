1541fbf1ad43e397fd38ccfad906a2c58c1fe411
gunmentoy <tomnguyen0443@gmail.com>
2023-11-12 21:45:19 -0800

    added a js function that converts weight (pounds) to three different outputs

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// making sure output cards don't show up on load
+document.getElementById('output').style.visibility = 'hidden';
+
+// get weight input
+document.getElementById('lbsInput').addEventListener('input', function(e){
+  document.getElementById("output").style.visibility = "visible";
+  let lbs = e.target.value;
+  // output grams
+  document.getElementById('gramsOutput').innerHTML = lbs/0.0022046;
+
+  // output kilograms
+  document.getElementById('kgOutput').innerHTML = lbs/2.2046;
+
+  // output ounces
+  document.getElementById('ozOutput').innerHTML = lbs*16;
+});
